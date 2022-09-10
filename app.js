5af761cdc3a2977f7ceffc1d498a97072aceaa53
“pravat” <koleyz1965@gmail.com>
2022-09-10 19:22:53 +0530

    Inital file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
-app.use(express.static(__dirname + '/public'));
+app.use("/public", express.static('./public/'));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
